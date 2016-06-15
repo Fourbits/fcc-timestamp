493e26e69dceea94ef6ceabaadd52649455d72f8
Fourbits <fourbits@gmail.com>
2016-06-15 14:46:57 +0000

    added default port/ip values

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var app = express()
-var port = process.env.PORT;
-var ip = process.env.IP;
+var port = process.env.PORT || 8080;
+var ip = process.env.IP || '127.0.0.1';
 
 var months = [
   'January',
